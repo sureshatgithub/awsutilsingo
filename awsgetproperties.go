802e976374a0e82fba756bcbc82fe8d4ea6387e3
Suresh Cheekatla <such@cisco.com>
2020-05-09 08:12:55 +0530

    fc

--- awsgetproperties.go
+++ awsgetproperties.go
@@ -6,7 +6,7 @@ import (
 	"github.com/magiconair/properties"
 )
 
-func getProperties(bucket string, fileName string) properties.Properties {
+func GetProperties(bucket string, fileName string) properties.Properties {
 	p := properties.MustLoadFile("config.properties", properties.UTF8)
 	fmt.Println(p.MustGetString("test"))
 	fmt.Println(p.MustGetString("result"))
